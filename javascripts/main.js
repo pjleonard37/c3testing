fa559823167e98ca239ee9c218921568cff4680e
PJ Leonard <patrick@messageagency.com>
2015-12-11 16:53:03 -0500

    bar

--- javascripts/main.js
+++ javascripts/main.js
@@ -60,6 +60,11 @@ console.log("loaded main.js");
         ],
         type: 'bar'
       },
+      axis: {
+        x: {
+          type: 'category',
+        }
+      },
       color: {
         pattern: ['#449AA2', '#364D6E', '#703F7D', '#ED871F', '#B5004A', '#744584', '#cfddb8', '#97AFD2']
       },
