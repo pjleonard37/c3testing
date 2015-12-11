bf1cad8085ae2163790c27989af555fe6201239b
PJ Leonard <patrick@messageagency.com>
2015-12-11 16:56:07 -0500

    stacked bar

--- javascripts/main.js
+++ javascripts/main.js
@@ -75,21 +75,32 @@ console.log("loaded main.js");
   var chart = c3.generate({
       bindto: '#stackedbar',
       data: {
+        x: 'x',
         columns: [
-            ['New York', 10],
-            ['Boston', 20],
-            ['Philadelphia', 30],
-            ['Providence', 40],
-            ['Baltimore', 50],
-            ['Washington D.C.', 60],
+          ['x', 'Bicyclists', 'Pedestrians', 'Cars', 'Boats'],
+          ['New York', 1000232, 1050232, 1100232, 1300232],
+          ['Boston', 1800232, 1200232, 1700232, 1100232],
+          ['Philadelphia', 1330232, 1870232, 2500232, 1065232]
         ],
-        type: 'stackedbar'
+        type: 'bar',
+        groups: [
+          ['New York', 'Boston']
+        ]
+      },
+      grid: {
+        y: {
+          lines: [{value:0}]
+        }
+      },
+      axis: {
+        x: {
+          type: 'category',
+        }
       },
       color: {
         pattern: ['#449AA2', '#364D6E', '#703F7D', '#ED871F', '#B5004A', '#744584', '#cfddb8', '#97AFD2']
       },
       legend: {
-          position: 'bottom'
+          position: 'right'
       }
   });
-})();
