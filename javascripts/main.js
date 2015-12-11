147be8619f89502bd6c7f5889f9f41e4246e25bd
PJ Leonard <patrick@messageagency.com>
2015-12-11 16:28:57 -0500

    duplicate

--- javascripts/main.js
+++ javascripts/main.js
@@ -21,4 +21,24 @@ console.log("loaded main.js");
           position: 'bottom'
       }
   });
+  var chart = c3.generate({
+      bindto: '#line',
+      data: {
+        columns: [
+            ['New York', 10],
+            ['Boston', 20],
+            ['Philadelphia', 30],
+            ['Providence', 40],
+            ['Baltimore', 50],
+            ['Washington D.C.', 60],
+        ],
+        type: 'pie'
+      },
+      color: {
+        pattern: ['#449AA2', '#364D6E', '#703F7D', '#ED871F', '#B5004A', '#744584', '#cfddb8', '#97AFD2']
+      },
+      legend: {
+          position: 'bottom'
+      }
+  });
 })();
