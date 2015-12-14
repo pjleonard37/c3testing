6c3d2a47e66b94c48460eded36077594df401545
PJ Leonard <patrick@messageagency.com>
2015-12-14 14:01:23 -0500

    repositioning

--- javascripts/main.js
+++ javascripts/main.js
@@ -35,8 +35,9 @@ console.log("loaded main.js");
         ]
       },
       regions: [
-        {start:'2013-01-01', end:'2013-12-31'},
-        {start:'2014-01-01', end:'2015-01-01', class:'foo'}
+        {start:'2013-01-01', end:'2014-01-01', class:'2013'},
+        {start:'2014-01-01', end:'2015-01-01', class:'2014'},
+        {start:'2015-01-01', end:'2016-01-01', class:'2015'}
       ],
       axis: {
         x: {
