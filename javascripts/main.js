f4368a34909a95e47ac4d75c0569c74998d7551c
PJ Leonard <patrick@messageagency.com>
2016-03-23 17:19:52 -0400

    typo

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,28 +1,17 @@
 console.log("loaded main.js");
 
+// ID of the Google Spreadsheet
+
+
+// Make sure it is public or set to Anyone with link can view
+
 (function() {
-  console.log("function");
-  var myChartType = "linechart";
-  var myChartID = "13SN1M6K1lbkmME6b5x1rigC2ps52w3NNqjMX7cnGTGU";
-  var myChartGid = 300047528;
-  var myxTitle = "xtitle";
-  var myyTitle = "ytitle";
-  var myLegend = "bottom";
-  var myDirection = "";
-  var myStacked = "";
-  var myFilled = "";
-  var randomId = "pie";
-  var colorvalue = ["#501010", "#901010", "#211010", "#108710", "#108710", "#101650", "#101056", "#101560"];
-  var thisColColor;
-  for (var i = 1; i < 9; i++) {
-    colorvalue.push($(this).data('col' + i));
-  }
-  var myURL = "https://spreadsheets.google.com/feeds/list/" + myChartID + "/1/public/values?alt=json";
-  //https://spreadsheets.google.com/feeds/list/13SN1M6K1lbkmME6b5x1rigC2ps52w3NNqjMX7cnGTGU/4/public/values?alt=json
-//  var myURL = "https://spreadsheets.google.com/feeds/list/" + myChartID + "/od6/public/values?gid=" + myChartGid + "&alt=json";
-  console.log(myURL);
-  $.getJSON(myURL, function (data) {
-    for (var i = 0; i < data.feed.entry.length; i++) {
+  console.log("function sent");
+  var spreadsheetID = "140f-u3YxCg3xlspx-xNWPbkXVPSHOOMopCWtsV-Wdao"
+  var url = "https://spreadsheets.google.com/feeds/list/" + spreadsheetID + "/od6/public/values?alt=json";
+  console.log(url)
+  $.getJSON(url,
+    function (data) {
       for (var i = 0; i < data.feed.entry.length; i++) {
           for (var key in data.feed.entry[i]) {
               if (data.feed.entry[i].hasOwnProperty(key) && key.substr(0,4) === 'gsx$') {
@@ -36,6 +25,7 @@ console.log("loaded main.js");
               }
           }
       }
+
       //C3 Formatting
       var datap = data.feed.entry;
       var datacategories = Object.keys(datap[0]);
@@ -57,111 +47,40 @@ console.log("loaded main.js");
         labelobject[emptyarray[0]] = emptyarray[emptyarray.length - 1];
         emptyarray = [];
       }
-      var rotated = false;
-      if (myDirection === 'Horizontal')
-      {
-        rotated = true;
-      }
-      if (myStacked === 'False') {
-        datacategories = [];
-      }
-      var linetype = 'line';
-      if (myFilled === 'True') {
-        linetype = 'area';
-      }
-      switch (myChartType) {
-        case 'piechart' :
-          var chart = c3.generate({
-            bindto: document.getElementById(randomId),
-            data: {
-              x: 'x',
-              columns: dataarray,
-              names: labelobject,
-              type: 'pie'
-            },
-            color: {
-              pattern: colorvalue
-            },
-            legend: {
-              position: myLegend
-            }
-          });
-          break;
-        case 'linechart' :
-          var chart = c3.generate({
-            bindto: document.getElementById(randomId),
-            data: {
-              x: 'x',
-              columns: dataarray,
-              names: labelobject,
-              type: linetype,
-              groups: [datacategories]
-            },
-            axis: {
-              x: {
-                label: {
-                  text: myxTitle,
-                  position: 'outer-center'
-                }
-              },
-              y: {
-                label: {
-                  text: myyTitle,
-                  position: 'outer-middle'
-                },
-                tick: {
-                    format: d3.format("$,.2r")
-                }
-              },
-              rotated: rotated
-            },
-            color: {
-              pattern: colorvalue
-            },
-            legend: {
-              position: myLegend
-            }
-          });
-          break;
-        case 'barchart' :
-          var chart = c3.generate({
-            bindto: document.getElementById(randomId),
-            data: {
-              x: 'x',
-              columns: dataarray,
-              names: labelobject,
-              type: 'bar',
-              groups: [datacategories]
-            },
-            axis: {
-              x: {
-                label: {
-                  text: myxTitle,
-                  position: 'outer-center'
-                }
-              },
-              y: {
-                label: {
-                  text: myyTitle,
-                  position: 'outer-middle'
-                }
+
+      //Chart Building
+      var chart = c3.generate({
+          bindto: '#chart',
+          data: {
+            x: 'x',
+            columns: dataarray,
+            names: labelobject,
+            type: 'bar'
+          },
+          axis: {
+            x: {
+              type: 'timeseries',
+              label: {
+                text: 'Investment Year',
+                position: 'outer-center'
               },
               tick: {
-                  format: d3.format("$,.2r")
-              },
-              rotated: rotated
+                format: '%Y'
+              }
             },
-            color: {
-              pattern: colorvalue
-            },
-            legend: {
-              position: myLegend
+            y: {
+              label: {
+                text: 'Infrastructure Investment',
+                position: 'outer-middle'
+              }
             }
-          });
-          break;
-        case 'tablechart' :
-          break;
-      }
-    }
-  });
-})(); // each function
+          },
+          color: {
+            pattern: ['#449AA2', '#364D6E', '#703F7D', '#ED871F', '#B5004A', '#744584', '#cfddb8', '#97AFD2']
+          },
+          legend: {
+              position: 'bottom'
+          }
+      });
+    });
+})();
